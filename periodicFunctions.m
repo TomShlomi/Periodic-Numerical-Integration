606e1cf56285682ee3ed030b3cd85cdf3e040f08
Tom Shlomi <41210998+TomShlomi@users.noreply.github.com>
2019-12-02 13:21:02 -0500

    Add files via upload

---
+++ periodicFunctions.m
@@ -0,0 +1,7 @@
+f1 = @(k)(@(x) cos(k*x))
+f2 = @(x)(sin(x/2))
+f3 = @(x)((sin(x/2)).^3)
+f4 = @(a)(@(x)1./(a-cos(x)))
+f5 = @(a)(@(x)(exp(a*cos(x))))
+f6 = @(x)(exp((cos(x)-1).*(cos(x) + 1)))
+f7 = @(x)(exp(-x.^2))
